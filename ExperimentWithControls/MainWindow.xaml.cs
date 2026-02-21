84184a932c7c569c46c8bba72913a4bfcf1ddf15
Ford Henley <info@fordswebhelp.com>
2026-02-21 15:28:06 -0500

    completed lesson

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace ExperimentWithControls
         {
             number.Text = numberTextBox.Text;
         }
-
+        {
         private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = !int.TryParse(e.Text, out int result);
@@ -36,9 +36,42 @@ namespace ExperimentWithControls
             number.Text = smallSlider.Value.ToString("0");
         }
 
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
 
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
         }
     }
 }
